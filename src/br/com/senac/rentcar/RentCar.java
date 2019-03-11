63a309777afe45fd19765ecac0f3b9eaba7dd4b8
ehxavier <ehxavier@gmail.com>
2019-03-11 07:00:39 -0300

    Comentários em classe principal.

--- src/br/com/senac/rentcar/RentCar.java
+++ src/br/com/senac/rentcar/RentCar.java
@@ -21,12 +21,13 @@ public class RentCar {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        /*
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (Exception ex) {
             JOptionPane.showMessageDialog(null, ex.getMessage());
         } 
-        
+        */
         FormLogin form = new FormLogin();
         form.setVisible(true);
     }
